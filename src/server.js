684ac52778bcca642c355dce504fcecbc9cbf8b4
Malik Iatazaz Altaf <malikiatazazaltaf@gmail.com>
2026-01-31 11:31:36 +0500

    ...

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ console.log("firstttttttttttttttttttttttttttt")
 // server.js
 const app = require('./app');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
